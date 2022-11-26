e39cda67c26c6857bc89256d4ea5a2b626dbc17c
Mahboobe Malekloo <mahboubeh.maleklou@gmail.com>
2022-11-26 11:54:32 +0330

    commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+import { authCodeFlowConfig } from './sso.config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'PWA';
+
+  constructor(private oauthservice:OAuthService){
+    this.configureSingleSignOn();
+  }
+
+  configureSingleSignOn(){
+    this.oauthservice.configure(authCodeFlowConfig);
+    this.oauthservice.tokenValidationHandler = new JwksValidationHandler;
+    this.oauthservice.loadDiscoveryDocumentAndTryLogin()
+  }
+
+  login(): void {
+    this.oauthservice.initImplicitFlow();
+  }
+
+  logout(): void {}
 }
