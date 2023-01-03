71258195c89df50478638f72cfa894c9dbde83a0
MahboubehMaleklou <mahboubeh.maleklou@gmail.com>
2023-01-03 14:21:01 +0330

    scan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+import { fromEvent, interval, map, mapTo, scan } from 'rxjs';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
   title = 'PWA';
+
+  constructor() {
+
+    // const scroll$ = fromEvent(document, 'scroll');
+    // scroll$.subscribe(console.log)
+    // scroll$.pipe(map(Scrolll => Scrolll.target)).subscribe(console.log)
+
+    const counter$ = interval(1000);
+
+    counter$
+      .pipe(
+        mapTo(-1),
+        scan((accu, curr) => {
+          return accu + curr;
+        }, 10)
+      )
+      .subscribe({
+        //next: console.log,
+      });
+  }
+
 }
